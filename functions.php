54dd36d72c3723c2c99401ac7aa5f140e5f95027
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2023-06-02 00:18:27 +0200

    commit 1

---
+++ functions.php
@@ -0,0 +1,8 @@
+<?php
+function openwebinarasthemeone_child_put_styles_and_scripts()
+{
+    wp_enqueue_style( 'style', get_stylesheet_uri() );
+}
+
+//add_action('wp_enqueue_scripts', 'openwebinarasthemeone_put_styles_and_scripts');
+add_action('wp_enqueue_scripts', 'openwebinarasthemeone_child_put_styles_and_scripts');
---
+++ index.php
@@ -0,0 +1,52 @@
+<?php
+/*
+* Template name: Inicio
+*/
+get_header('secondary'); ?>
+<?php get_sidebar(); ?>
+<div class="container">
+    <?php get_header('head2'); ?>
+    <?php if (is_front_page()): ?>
+        <section class="row">
+            <?php if (have_posts()): ?>
+                <?php while (have_posts()): ?>
+                    <?php the_post(); ?>
+                    <article class=" articulo col-md-3 col-sm-6">
+                        <picture class="thumbnail">
+                            <?php if (has_post_thumbnail()): ?>
+                                <?php the_post_thumbnail(array(242, 200)); ?>
+                            <?php else: ?>
+                                <img width="200" height="200" src="https://img.interempresas.net/fotos/2733357.png"
+                                     alt="">
+                            <?php endif ?>
+                            <div class="caption">
+                                <a href="<?php the_permalink(); ?>">
+                                    <h3 class="justify-center">
+                                        <?php the_title(); ?>
+                                    </h3>
+                                </a>
+                            </div>
+                            <p class="text-center">
+                                <?php substr(the_excerpt(), 0, 200) ?></p>
+                            <p><a href="#" class="btn btn-primary">Leer más</a></p>
+                        </picture>
+                    </article>
+                <?php endwhile ?>
+            <?php else:
+                __("Lo sentimos. No hay posts disponibles", "openwebinarsthemeone");
+                ?>
+
+            <?php endif ?>
+        </section>
+    <?php elseif ( is_singular() || is_page() ): ?>
+
+        <?php get_single_template(); ?>
+
+    <?php else: ?>
+
+        <?php __("Lo sentimos. No hay posts disponibles 2", "openwebinarsthemeone"); ?>
+
+
+
+    <?php endif; ?>
+    <?php get_footer(); ?>
